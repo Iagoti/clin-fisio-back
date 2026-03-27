e96960a690a36a3d37da30ec261e22b7cfb98711
Iagoti <iagoti03@gmail.com>
2026-03-26 22:34:28 -0300

    fix: add metodo findAll

--- backend/src/main/java/com/system/fisio/infrastructure/persistence/repository/UsuarioJpaRepository.java
+++ backend/src/main/java/com/system/fisio/infrastructure/persistence/repository/UsuarioJpaRepository.java
@@ -1,11 +1,14 @@
 package com.system.fisio.infrastructure.persistence.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.system.fisio.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.system.fisio.infrastructure.persistence.entity.UsuarioEntity;
 
 public interface UsuarioJpaRepository extends JpaRepository<UsuarioEntity, Integer> {
     Optional<UsuarioEntity> findByEmail(String email);
+    List<UsuarioEntity> findAll();
 }
